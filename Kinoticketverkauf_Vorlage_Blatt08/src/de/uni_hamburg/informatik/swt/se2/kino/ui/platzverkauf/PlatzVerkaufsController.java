febe4935142a60bf13a87818f5ac994f1b6fb079
654Lukas456 <lukas.hirschle@gmail.com>
2025-07-10 18:08:46 +0200

    TODO

--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/ui/platzverkauf/PlatzVerkaufsController.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/ui/platzverkauf/PlatzVerkaufsController.java
@@ -203,6 +203,7 @@ public class PlatzVerkaufsController
     {
         Set<Platz> plaetze = _view.getPlatzplan().getAusgewaehltePlaetze();
         
+        //TODO Richtige Geldbeträge reingeben Buttons gehen, Modal
         BezahlController bezahlcontroller = new BezahlController(Geldbetrag.get(1000));
         
         vorstellung.verkaufePlaetze(plaetze);
